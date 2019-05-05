69b944b18725234570d8d6cab63ef8d1bb56a3f4
bouzuya <m@bouzuya.net>
2019-05-05 14:15:02 +0900

    add Message

--- spago.dhall
+++ spago.dhall
@@ -5,7 +5,8 @@ You can edit this file as you like.
 { name =
     "my-project"
 , dependencies =
-    [ "console"
+    [ "bouzuya-datetime-formatter"
+    , "console"
     , "effect"
     , "httpure"
     , "node-sqlite3"
--- src/Type.purs
+++ src/Type.purs
@@ -1,12 +1,46 @@
 module Type
   ( DB
+  , Message
+  , Timestamp
   , User
+  , timestampToString
   ) where
 
+import Prelude
+
+import Bouzuya.DateTime.Formatter.DateTime as DateTimeFormatter
+import Data.DateTime (DateTime)
+import Data.Maybe as Maybe
+import Foreign as Foreign
+import Simple.JSON (class ReadForeign, class WriteForeign)
+import Simple.JSON as SimpleJSON
+
+newtype Timestamp = Timestamp DateTime
+
+instance readForeignTimestamp :: ReadForeign Timestamp where
+  readImpl f = do
+    s <- SimpleJSON.readImpl f
+    case DateTimeFormatter.fromString s of
+      Maybe.Nothing -> Foreign.fail (Foreign.ForeignError "Timestamp")
+      Maybe.Just dt -> pure (Timestamp dt)
+
+instance writeForeignTimestamp :: WriteForeign Timestamp where
+  writeImpl t = SimpleJSON.writeImpl (timestampToString t)
+
 type DB = String
+
+type Message =
+  { created_at :: Timestamp
+  , message :: String
+  , user_id :: String
+  , id :: String
+  }
 
 type User =
   { id :: String
   , name :: String
   , url :: String
   }
+
+timestampToString :: Timestamp -> String
+timestampToString (Timestamp dt) = DateTimeFormatter.toString dt
